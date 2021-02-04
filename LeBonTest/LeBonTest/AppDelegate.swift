c4a8ebe1db23b2102fcbee335aaa72b265d232e7
Stéphane AZZOPARDI <contact@stephane-azzopardi.fr>
2021-02-04 10:34:23 +0100

    Init Xcode project

---
+++ LeBonTest/LeBonTest/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  LeBonTest
+//
+//  Created by Stéphane Azzopardi on 04/02/2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ LeBonTest/LeBonTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LeBonTest
+//
+//  Created by Stéphane Azzopardi on 04/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
